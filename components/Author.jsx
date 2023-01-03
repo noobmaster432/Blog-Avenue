546e950259343b755f07ce201f3ba3a0e2a045c0
Gyanendra Tiwari <mrgyan432@gmail.com>
2023-01-03 19:37:57 +0530

    some changes

--- components/Author.jsx
+++ components/Author.jsx
@@ -15,7 +15,7 @@ const Author = ({ author }) => {
         />
       </div>
       <h3 className="text-white my-4 text-xl font-bold">{author.name}</h3>
-      <p className="text-white text-lg">{author.bio}</p>
+      <p className="text-white text-lg font-sans">{author.bio}</p>
     </div>
   );
 };
